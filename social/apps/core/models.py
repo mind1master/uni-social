3516de978ce1a6baf6db4313bada39955d8e659e
igork93 <igor.kondratiev93@gmail.com>
2013-05-18 15:35:17 +0300

    issue:1 User model rewrited: blank added

--- social/apps/core/models.py
+++ social/apps/core/models.py
@@ -6,6 +6,6 @@ class SocialProfile(models.Model):
     user = models.OneToOneField(User)
 
     date_of_birth = models.DateField(null=True)
-    home_town = models.CharField(max_length=20, null=True)
-    phone_number = models.CharField(max_length=10, null=True)
-    skype_id = models.CharField(max_length=20, null=True)
+    home_town = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=10, blank=True)
+    skype_id = models.CharField(max_length=20, blank=True)
