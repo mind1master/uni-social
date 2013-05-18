eedf6f9b7ea57fc9dfe8c3baaa90fa2c1b3f0ced
Anton Kasyanov <mind1master@gmail.com>
2013-05-18 16:29:37 +0300

    admin added for posts

--- social/apps/core/admin.py
+++ social/apps/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from social.apps.core.models import SocialProfile
+from social.apps.core.models import SocialProfile, AnyPost
 
 class UserInline(admin.StackedInline):
     model = SocialProfile
@@ -12,6 +12,11 @@ class UserInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(UserAdmin):
     inlines = (UserInline, )
+
+class AnyPostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(AnyPost, AnyPostAdmin)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
